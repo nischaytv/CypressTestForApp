f8da32681750226c5ba8cfa1d266a22b73060ac3
nischaytv <rocknischay@gmail.com>
2022-05-12 00:01:11 +0530

    Update NisTests.spec.js

--- cypress/integration/NisTests.spec.js
+++ cypress/integration/NisTests.spec.js
@@ -5,7 +5,7 @@ describe('Testing Nischays App', ()=>{
     it('Verify the name in h1 tag',()=>{
 
         cy.visit('https://nischay.azurewebsites.net/webapp/')
-        cy.get('body h1').should('have.text',' Nischay1 ')
+        cy.get('body h1').should('have.text',' Nischay11 ')
         cy.get('body h2').should('have.text',' Buy me a coffee and get your name featured above! ')
     })
 })
